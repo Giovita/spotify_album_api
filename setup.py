db81a4424fb1b0f25292e0e78e76e9ea2977dd3d
Santiago Giovagnoli <santigiova@gmail.com>
2021-11-15 14:31:23 -0300

    Add Makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='SPOTIFY_ALBUM_API',
+      version="1.0",
+      description="List every album for a given artist in spotify by calling its public API",
+      packages=find_packages(),
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      install_requires=requirements)
