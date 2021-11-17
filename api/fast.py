01aee94dca4d0094d6ed09f2ee517ebd8be66bb8
Santiago Giovagnoli <santigiova@gmail.com>
2021-11-17 19:45:58 -0300

    Page results and return album list from fast.py

--- api/fast.py
+++ api/fast.py
@@ -64,33 +64,57 @@ def get_artist(q):
     artist_id = resp['artists']['items'][0]['id']
     artist_name = resp['artists']['items'][0]['name']
         
-    # return artist_name, artist_id 
+    return artist_name, artist_id 
     
 @app.get('/artist-albums')
-def get_artist_albums():
+def get_artist_albums(artist):
     """
     From artist ID, return a list of every album of the artist from Spotify's API
     https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artists-albums
     """
     
-    artist = "3WrFJ7ztbogyGnTHbHJFl2"
-    url = urljoin(URI, f'artists/{artist}/albums?include_groups=album&offset=10')  # Filters 
+    # artist = "3WrFJ7ztbogyGnTHbHJFl2"  # The Beatles
+    # artist = "0k17h0D3J5VfsdmQ1iZtE9"  # Pink Floyd
     header = {'Authorization': f'Bearer {auth_token}', 
               'Content-Type': 'application/json'}
+    # url = urljoin(URI, f'artists/{artist}/albums?include_groups=album&offset=10')  # Filters only "album" types
+
+    # resp = requests.get(url, headers=header).json()
+
+    # albums = []
+    # for album in resp.get('items'):
+    #     albums.append(album['name'])
+
+    # # return albums
+    # if resp.get('items'):
+    #     is_empty = False
+    # elif not resp.get('items'):
+    #     is_empty = True
+        
+    page = 0
+    albums = []
+    url = urljoin(URI, f'artists/{artist}/albums?include_groups=album&limit=49&offset=0')  # Filters only "album" types
 
     resp = requests.get(url, headers=header).json()
+    while resp.get('items'):
+        url = urljoin(URI, f'artists/{artist}/albums?include_groups=album&limit=49&offset={page}')   
+        # url = urljoin(URI, f'artists/{artist}/albums?&limit=49&offset={page}')   
+        resp = requests.get(url, headers=header).json()
+        for album in resp.get('items'):
+            albums.append(album['name'])
 
-    albums = []
-    for album in resp.get('items'):
-        albums.append(album['name'])
-
-    # return albums
-    if resp.get('items'):
-        is_empty = False
-    elif not resp.get('items'):
-        is_empty = True
+        page += 50
     
-    return is_empty 
+    # url = urljoin(URI, f'artists/{artist}/albums?limit=10')  # Filters only "album" types
+    # resp = requests.get(url, headers=header).json()
+    # for album in resp.get('items'):
+    #     albums.append(album['name'])
+    
+    
+    print(len(albums))
+    
+    next = resp['next']
+    return albums
     
     # artist_url = urljoin(URI, f'search?q={q}&type=artist')
     
